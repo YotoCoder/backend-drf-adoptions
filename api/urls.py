8e2b1636181e70d7733a3f7a52aaeff22b6c2937
YotoCoder <yotoelectronics@gmail.com>
2022-09-18 22:47:41 -0500

    llll

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import PetTestView, PetView
-
-router = DefaultRouter()
-
-router.register('api/pet', PetView, 'petview')
-router.register('api/test', PetTestView, 'test')
-
-urlpatterns = router.urls
