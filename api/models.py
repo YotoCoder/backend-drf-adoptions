ff40c7e18667bc665ee315faea6118b763bb5c09
YotoCoder <yotoelectronics@gmail.com>
2022-09-18 21:05:51 -0500

    models_features

--- api/models.py
+++ api/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 class Pet(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=255)
     img = models.ImageField(upload_to='img/pets/')
+    age = models.IntegerField(max_length=1)
+    sex = models.CharField(max_length=10)
+    size = models.IntegerField(max_length=2)
+    city = models.CharField(max_length=66)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -13,6 +18,7 @@ class Pet(models.Model):
     def __str__(self) -> str:
         return self.name
 
+# Test Pet
 class ModelPetTest(models.Model):
     name = models.CharField(max_length=10)
     img = models.ImageField(upload_to='img/petstes/', blank=True, null=True)
