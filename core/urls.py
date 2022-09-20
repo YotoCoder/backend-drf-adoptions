61cb87dff18aa79ab346921a59a6448dbcd88ce0
YotoCoder <yotoelectronics@gmail.com>
2022-09-20 15:48:06 -0500

    auth user model

--- core/urls.py
+++ core/urls.py
@@ -33,5 +33,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # user profile routes
+    path('api/user', include('user.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
