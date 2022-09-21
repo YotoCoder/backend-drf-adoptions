08aca42a7a93e20fc70295ac40adc902c3ce8237
YotoCoder <yotoelectronics@gmail.com>
2022-09-21 02:48:29 -0500

    envio de email ok

--- email_recuperation/models.py
+++ email_recuperation/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Key(models.Model):
-    is_active = models.BooleanField()
-    
+class Secret(models.Model):
+    key = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
--- email_recuperation/views.py
+++ email_recuperation/views.py
@@ -1,33 +1,31 @@
 import os
-from django.shortcuts import render
+import secrets
 from django.core.mail import send_mail
 from django.conf.global_settings import EMAIL_HOST_USER
 from rest_framework.response import Response
 from user.models import User
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from .crypto import Crypto
+from .models import Secret
 
 
+class RecoverPassword(APIView): 
 
+    link = os.environ.get('HOST_FRONT')
 
-# Recibe un email lo busca y si se encuentra le envia el link de restauración al correo
-
-class RecoverPassword(APIView):
-
-    link = os.environ.get('HOST')
-
-    def post(self, request):
+    def post(self, request, *args, **kwargs): # Recibe un email lo busca y si se encuentra le envia el link de restauración al correo
 
         try:
-            email = request.POST['email']
+            email = request.data['email']
             email = User.objects.filter(email=email).values('email')[0]['email']
+            
+            token = secrets.token_urlsafe(20)
 
             send_mail(
                 'Recuperación de contraseña.',
-                f'Por favor ingrese a este link {self.link} para reestablecer su contraseña.',
+                f'Por favor ingrese a este link {self.link}/{token} para reestablecer su contraseña.',
                 EMAIL_HOST_USER,
                 [email],
                 fail_silently=False,
@@ -41,7 +39,12 @@ class RecoverPassword(APIView):
         except Exception as e:
             return Response(
                     {
-                        'message':'Lo sentimos no tiene ningun correo registrado, comuniquese con el area de soporte!'
+                        'message':'Lo sentimos no tiene ningun correo registrado, comuniquese con el area de soporte!',
                     },
                 status=HTTP_400_BAD_REQUEST
             )
+
+class UpdatePasswors(APIView):
+
+    def get(self, request):
+        pass
