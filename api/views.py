69a549378fc7c99e06b7852e07143bb5b8f364bf
YotoCoder <yotoelectronics@gmail.com>
2022-09-25 10:46:46 -0500

    views_mods

--- api/views.py
+++ api/views.py
@@ -15,3 +15,17 @@ class PetTestView(viewsets.ModelViewSet):
     queryset = ModelPetTest.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = PetTestSerializer
+
+
+# Crear una vista con el endpoint /api/pet/filter que permita filtrar con rangos de edad, sexo y tamaño
+
+class PetFilterView(generics.ListAPIView):
+    queryset = Pet.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PetSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'age': ['gte', 'lte',],
+        'sex': ['contains'],
+        'size': ['gte', 'lte']
+    }
