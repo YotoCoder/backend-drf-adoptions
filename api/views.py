0bc7bd0f41921946b4d2e151cc227e813679daf5
YotoCoder <yotoelectronics@gmail.com>
2022-10-05 17:59:25 -0500

    feature/pagination

--- api/views.py
+++ api/views.py
@@ -5,11 +5,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from .paginations import PetPagination
+
 class PetView(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PetSerializer
     filter_backends = [DjangoFilterBackend]
+    pagination_class = PetPagination
 
     filterset_fields = {
         'age': ['gt', 'lt', 'exact'],
