657dab2dda3430417e502b85fc092582a81032a4
YotoCoder <yotoelectronics@gmail.com>
2022-10-05 21:17:03 -0500

    default changes

--- api/models.py
+++ api/models.py
@@ -4,12 +4,12 @@ from user.models import User
 # Create your models here.
 
 CHOICES_AGE = (
-    (1, 1),
-    (2, 2),
-    (3, 3),
-    (4, 4),
-    (5, 5),
-    (6, 6),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
 )
 
 CHOICES_SEX = (
@@ -18,9 +18,9 @@ CHOICES_SEX = (
 )
 
 CHOICES_SIZE = (
-    (1, 1),
-    (2, 2),
-    (3, 3),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
 )
 
 CHOICES_TYPE_PET = (
@@ -34,9 +34,9 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=255)
     img = models.ImageField(upload_to='img/pets/')
-    age = models.IntegerField(default=1, choices=CHOICES_AGE)
-    sex = models.CharField(max_length=10, default=2, choices=CHOICES_SEX)
-    size = models.IntegerField(default=2, choices=CHOICES_SIZE)
+    age = models.CharField(max_length=1, choices=CHOICES_AGE)
+    sex = models.CharField(max_length=10, choices=CHOICES_SEX)
+    size = models.CharField(max_length=1, choices=CHOICES_SIZE)
     type_pet = models.CharField(max_length=10, default='Perro', choices=CHOICES_TYPE_PET)
     city = models.CharField(max_length=66, default='Lima')
     is_active = models.BooleanField(default=True)
