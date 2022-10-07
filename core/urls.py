e826149054a852fc7cdf39e3daf9476c18342834
YotoCoder <yotoelectronics@gmail.com>
2022-10-06 20:56:03 -0500

    menu con jazzmin :$

--- core/urls.py
+++ core/urls.py
@@ -48,4 +48,4 @@ urlpatterns = [
     # Debug toolbar
     path('__debug__/', include('debug_toolbar.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
